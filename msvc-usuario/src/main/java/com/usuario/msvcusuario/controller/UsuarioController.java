302518f0cb5629f184b7ddcdb41c3dbaa60d7c29
damian <adamodamian6@gmail.com>
2023-09-19 21:23:24 -0300

    a

--- msvc-usuario/src/main/java/com/usuario/msvcusuario/controller/UsuarioController.java
+++ msvc-usuario/src/main/java/com/usuario/msvcusuario/controller/UsuarioController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
+@RequestMapping("")
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
