685b76ac0b940f2c7aa5fad42a9a0b913174ae0d
damian <adamodamian6@gmail.com>
2023-09-24 12:36:53 -0300

    emprezando a darle forma al microservicios proceso

---
+++ msvc-proceso/src/main/java/com/procesos/demo/entity/ProcesoEmprendedor.java
@@ -0,0 +1,10 @@
+package com.procesos.demo.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "procesoEmprendedor")
+public class ProcesoEmprendedor {
+    @Id
+    private Long id;
+}
---
+++ msvc-proceso/src/main/java/com/procesos/demo/entity/modelo/Cliente.java
@@ -0,0 +1,5 @@
+package com.procesos.demo.entity.modelo;
+
+public class Cliente {
+    private Long id;
+}
