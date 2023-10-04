04b9be87c641c953144fecd433ca0ca8660ad2de
damian <adamodamian6@gmail.com>
2023-10-04 18:42:52 -0300

    no dr

---
+++ msvc-cliente/src/main/java/com/cliente/msvccliente/repository/MunicipioRepositorio.java
@@ -0,0 +1,7 @@
+package com.cliente.msvccliente.repository;
+
+import com.cliente.msvccliente.entity.Municipio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MunicipioRepositorio extends JpaRepository<Municipio,Long> {
+}
