abbb49c1ee67f5cfb8003bd9379c1b789f210802
damian <adamodamian6@gmail.com>
2023-10-31 20:46:44 -0300

    un sola ruta para guardar proceso Emprendedor

--- msvc-proceso/src/main/java/com/procesos/demo/entity/ProcesoEmprendedor.java
+++ msvc-proceso/src/main/java/com/procesos/demo/entity/ProcesoEmprendedor.java
@@ -5,16 +5,12 @@ import com.procesos.demo.entity.emprendedor.canvas.Canvas;
 import com.procesos.demo.entity.emprendedor.modelobasico.PlanNegocio;
 import com.procesos.demo.entity.emprendedor.modelofinanciero.PlanFinanciero;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.data.mongodb.core.mapping.FieldType;
 
-@Document(collection = "procesoEmprendedor")
+
+
 @Data
 public class ProcesoEmprendedor {
-    @Field(targetType = FieldType.OBJECT_ID)
-    private String id;
+
     private String nombre;
 
     private AutoEvaluacion autoEvaluacion;
