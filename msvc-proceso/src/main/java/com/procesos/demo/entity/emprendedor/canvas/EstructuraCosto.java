cb0f4c7e4118ff6083ebeb79f1ef62429fbdce5e
damian <adamodamian6@gmail.com>
2023-11-28 19:46:04 -0300

    no se

--- msvc-proceso/src/main/java/com/procesos/demo/entity/emprendedor/canvas/EstructuraCosto.java
+++ msvc-proceso/src/main/java/com/procesos/demo/entity/emprendedor/canvas/EstructuraCosto.java
@@ -2,8 +2,7 @@ package com.procesos.demo.entity.emprendedor.canvas;
 
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.data.mongodb.core.mapping.FieldType;
+
 
 import java.util.List;
 
