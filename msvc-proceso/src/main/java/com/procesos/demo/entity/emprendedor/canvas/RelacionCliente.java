9a137cda19452af54d2b94f8088874ac7c7812ee
damian <adamodamian6@gmail.com>
2023-12-01 19:40:08 -0300

    todos los canvas sin relacion a tablas

--- msvc-proceso/src/main/java/com/procesos/demo/entity/emprendedor/canvas/RelacionCliente.java
+++ msvc-proceso/src/main/java/com/procesos/demo/entity/emprendedor/canvas/RelacionCliente.java
@@ -1,10 +1,8 @@
 package com.procesos.demo.entity.emprendedor.canvas;
 
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.data.mongodb.core.mapping.FieldType;
 
-@Document(collection = "relacion_clientes")
+
+
 public class RelacionCliente {
 
     private String captacion;
